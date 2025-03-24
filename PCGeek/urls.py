eb385132e8777153f2c8023f6d60e8f14df2a266
nischal-0016 <koiralanischal16@gmail.com>
2025-03-24 15:19:33 +0545

    model

--- PCGeek/urls.py
+++ PCGeek/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from store import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
